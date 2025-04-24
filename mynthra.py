48a3a6d8db825330f2337620c2c24ea9ce5af981
Ubuntu <ubuntu@ip-172-31-41-218.eu-west-2.compute.internal>
2025-04-24 07:34:23 +0000

    added few doc inst comm

---
+++ mynthra.py
@@ -0,0 +1,6 @@
+2  MAINTAINER (Deprecated)
+•	Purpose: Used to specify the author/maintainer of the image (deprecated in favor of LABEL).
+•	Syntax:
+MAINTAINER Name <email@example.com>
+•	Example:
+MAINTAINER param <gparameswara99@gmail.com>
