02b63ea58bfd38ab05bf9b66e069db9c257fcf77
Narmada1821 <nandurinarmada1821@gmail.com>
2025-04-24 14:21:37 +0530

    added doc inst run file

--- flipkart.java
+++ flipkart.java
@@ -5,3 +5,12 @@ FROM openjdk:11-jre-slim
 RUN mkdir /opt/tomcat
 
 WORKDIR /opt/tomcat
+3  RUN
+•	Purpose: Executes a command during the image build process.
+•	Syntax:
+
+RUN <command>
+•	Example:
+
+RUN apt-get update && apt-get install -y curl
+This updates the package list and installs curl.
